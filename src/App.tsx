c6aac419a0902a82aab00fbc7b9d4b3295f9165f
Berow <52027524+Berow@users.noreply.github.com>
2021-04-04 22:44:38 +0300

    map

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="Map">
       <MapContainer center={[0, 0]} zoom={4}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors"/>
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
       </MapContainer>
     </div>
   );
